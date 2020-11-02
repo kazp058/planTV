3f918fb5e91c240ae6ea2b43c88dacef1029db5e
kazp058 <kazp058@gmail.com>
2020-11-02 13:52:53 -0500

    Cfg implemented and redifinition of save and read

--- toolkit.py
+++ toolkit.py
@@ -1,37 +1,52 @@
 import json
 import os
-import config as cfg
+from config import node_save_filename, data_save_filename, file_extension, test_node_save_filename, test_data_save_filename
+from node import *
 
 path = os.getcwd()
 
+
 def save_devices(devices, filename):
     with open(path + "/saves/" + filename, 'w+') as node_file:
-         for device_id in devices.keys():
-             cache = device_id + "\n"
-             node_file.write(cache)
+        for device_id in devices.keys():
+            cache = device_id + "\n"
+            node_file.write(cache)
+
 
 def save_data(devices, filename):
     pass
 
-def save(devices, test = False):
-    extension = cfg.file_extension
 
+def save(devices, test=False):
     if test == False:
-       save_devices(devices, cfg.node_save_filename + "." + extesion)
-       save_data(devices, cfg.data_save_filename + "." + extension)
+        save_devices(devices, node_save_filename + "." + file_extension)
+        save_data(devices, data_save_filename + "." + file_extension)
     else:
-       save_devices(devices, cfg.test_node_save_filename + "." + extension)
-       save_data(devices, cfg.test_data_save_filename + "." + extension)
+        save_devices(devices, test_node_save_filename + "." + file_extension)
+        save_data(devices, test_data_save_filename + "." + file_extension)
 
-def read_devices(filename):
-    with open("nodes.json",'r') as node_file:
-         cache = node_file.read()
-         return json.loads(cache)
+
+def read_devices(filename=node_save_filename):
+    cache = {}
+    with open(path + "/saves/" + filename + "." + file_extension, 'r') as node_file:
+        for line in node_file:
+            address = line.strip().split(".")
+            current = cache
+            for node_id in address:
+                if node_id not in current.keys():
+                    current[node_id] = dict()
+                    current = current[node_id]
+                else:
+                    current = current[node_id]
+
+            print(current)
+            node_id = line[0]
+            subnodes = {}
+            for i in range(len(line[1:])):
+                subnodes[Node(i)] = dict()
+
+    return cache
+
 
 if __name__ == "__main__":
-   print(path)
-   test_sample = {"1":{"temperature":29,"pressure":45,"humidity":95,"altitude":122,"luminocity":18},
-                  "2":{"temperature":31,"pressure":50,"humidity":78,"altitude":102,"luminocity":20}}
-   save(test_sample, test = True)
-
-
+    pass
